ubports/telegram-app

commit 46fbb9790d4bfb8545306dcae03363ebd2fe7316

Improve commit 7ef5958

telegram/app/qml/AccountSendMessage.qml
@@ -168,7 +168,7 @@ Rectangle {
 
         anchors {
             left: privates.recording ? anchorPoint.right: parent.left
-            right: focus ? send_button_box.left : sticker_button_box.left
+            right: focus && (text.length > 0 || txt.inputMethodComposing) ? send_button_box.left : sticker_button_box.left
             bottom: parent.bottom
             margins: units.gu(1)
             rightMargin: 0
@@ -290,7 +290,7 @@ Rectangle {
 
         opacity: privates.buttonsOpacity
         Behavior on opacity { UbuntuNumberAnimation {} }
-        visible: opacity > 0 && !messagePlaceholder.visible && !txt.focus
+        visible: opacity > 0 && !messagePlaceholder.visible && (!txt.focus || (txt.text.length == 0 && !txt.inputMethodComposing))
 
         AbstractButton {
             anchors.fill: parent
@@ -330,7 +330,7 @@ Rectangle {
 
         opacity: privates.buttonsOpacity
         Behavior on opacity { UbuntuNumberAnimation {} }
-        visible: opacity > 0 && !messagePlaceholder.visible && !txt.focus
+        visible: opacity > 0 && !messagePlaceholder.visible && (!txt.focus || (txt.text.length == 0 && !txt.inputMethodComposing))
 
         AbstractButton {
             anchors.fill: parent

commit b2bb74722ac45a2af5bf8f4dab39540644f32521

Fix regressions introduced in commit 46fbb97

telegram/app/qml/AccountSendMessage.qml
@@ -31,6 +31,9 @@ Rectangle {
         txt.focus = false;
         if (privates.emojiItem)
             privates.emojiItem.destroy();
+        if (privates.attachmentItem &&
+            privates.attachmentItem.isShown)
+            privates.attachmentItem.isShown = false;
     }
 
     function checkForSharedContent() {
@@ -299,6 +302,7 @@ Rectangle {
                 if (!telegramObject.connected || !Connectivity.online) return
 
                 if (!privates.emojiItem) {
+                    smsg.focusOut();
                     privates.emojiItem = emoticons_component.createObject(send_msg)
                     privates.emojiItem.y = -privates.emojiItem.height
                 } else {
@@ -340,7 +344,9 @@ Rectangle {
 
                 Haptics.play()
                 if (!privates.attachmentItem) {
-                    privates.attachmentItem = attach_panel_component.createObject(smsg)
+                    var txtWasFocused = txt.focus;
+                    smsg.focusOut();
+                    privates.attachmentItem = attach_panel_component.createObject(smsg, { isShown: txtWasFocused })
                 }
                 privates.attachmentItem.isShown = true;
             }

telegram/app/qml/components/AttachPanel.qml
@@ -59,6 +59,10 @@ Item {
         Rectangle {
             anchors.fill: parent
             color: "#aaffffff"
+
+            MouseArea {
+                anchors.fill: parent
+            }
         }
 
         InverseMouseArea {