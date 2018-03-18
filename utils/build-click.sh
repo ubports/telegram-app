62d6aed5121d90c518ad40c90e027fd2641ec0d5
Florian Leeber <flori@bin.org.in>
2018-03-19 00:34:42 +0100

    My somewhat working build ;)

--- utils/build-click.sh
+++ utils/build-click.sh
@@ -4,116 +4,83 @@ TELEGRAM_SOURCES=$(dirname "$(readlink -f "${0}")")/..
 CLICK_TARGET_DIR="$TELEGRAM_SOURCES/bin/ubuntu-touch/tmp" # tmp is hard-coded into clickable
 BUILD_DIR_BASENAME=build_mobile
 # modifications to g++.conf
-QMAKE_CC=arm-linux-gnueabihf-gcc
-QMAKE_CXX=arm-linux-gnueabihf-g++
-QMAKE_LINK=arm-linux-gnueabihf-g++
-QMAKE_LINK_SHLIB=arm-linux-gnueabihf-g++
-QMAKE_BIN="/usr/lib/qt5/bin/qmake"
+export QMAKE_CC=arm-linux-gnueabihf-gcc
+export QMAKE_CXX=arm-linux-gnueabihf-g++
+export QMAKE_LINK=arm-linux-gnueabihf-g++
+export QMAKE_LINK_SHLIB=arm-linux-gnueabihf-g++
+export QMAKE_BIN="/usr/lib/qt5/bin/qmake"
 QT_SELECT=qt5
 MAKE_BIN=make
 
 mkdir -p $CLICK_TARGET_DIR
 
-ARCH=$(dpkg-architecture -qDEB_HOST_ARCH)
-DEB_HOST_MULTIARCH=$(dpkg-architecture -qDEB_HOST_MULTIARCH)
-
-install_deb() {
-	BASE_URL="${1}"; PKG="${2}"; VERSION="${3}"
-	DEB_NAME="${PKG}_${VERSION}_${ARCH}.deb"
-
-	# download deb using curl with a nice progress bar
-	curl -L --progress-bar ${BASE_URL}/${DEB_NAME} -o "/tmp/${DEB_NAME}"
-	# install to click
-	dpkg-deb -x "/tmp/${DEB_NAME}" ${CLICK_TARGET_DIR}
-	# clean up
-	rm "/tmp/${DEB_NAME}"
-}
-
-install_dependencies() {
-
-echo "nothing do be done currently..."
-
-}
-
 build_libqtelegram() {
-echo "Calling qmake"
+
+echo "*****************************************"
+echo "Building libqtelegram-ae"
+echo "*****************************************"
+
 cd $TELEGRAM_SOURCES/deps/libqtelegram-ae
 mkdir -p $BUILD_DIR_BASENAME && cd $BUILD_DIR_BASENAME || exit 1
 # FIXME (rmescandon): workaround for letting yakkety desktop version compile. Seems that leaving
 # QMAKE_CFLAGS_ISYSTEM to default /usr/include yields stdlib.h error. Instead it is set to nothing
-$QMAKE_BIN -Wall PREFIX=/usr -r .. QMAKE_CFLAGS_ISYSTEM= || exit 1
-# $QMAKE_BIN PREFIX=/usr -r .. QMAKE_CFLAGS_ISYSTEM= CONFIG+=debug || exit 1 //for debugging
+$QMAKE_BIN -Wall PREFIX=/ -r .. QMAKE_CFLAGS_ISYSTEM= || exit 1
+# $QMAKE_BIN PREFIX=/ -r .. QMAKE_CFLAGS_ISYSTEM= CONFIG+=debug || exit 1 //for debugging
 echo "Calling make"
 $MAKE_BIN -j4 || exit 1
 $MAKE_BIN INSTALL_ROOT=$CLICK_TARGET_DIR install || exit 1
-cd $TG_DIR/deps
-
-
 }
 
 build_TelegramQML() {
 
+echo "*****************************************"
 echo "Building TelegramQML"
+echo "*****************************************"
 
 cd $TELEGRAM_SOURCES/deps/TelegramQML
 mkdir -p $BUILD_DIR_BASENAME && cd $BUILD_DIR_BASENAME || exit 1
 # FIXME (rmescandon): workaround for letting yakkety desktop version compile. Seems that leaving
 # QMAKE_CFLAGS_ISYSTEM to default /usr/include yields stdlib.h error. Instead it is set to nothing
 $QMAKE_BIN \
-    LIBS+=-L$TG_LIBS LIBS+=-lqtelegram-ae \
-    LIBQTELEGRAM_INCLUDE_PATH+=$TG_INCS/libqtelegram-ae \
-    LIBS+=-L$TH_LIBS LIBS+=-lthumbnailer-qt \
-    INCLUDEPATH+=$TH_INCS \
+    LIBS+=-L$CLICK_TARGET_DIR/lib/x86_64-linux-gnu LIBS+=-lqtelegram-ae \
+    LIBQTELEGRAM_INCLUDE_PATH+=$CLICK_TARGET_DIR/include/libqtelegram-ae \
+    LIBS+=-lthumbnailer-qt \
+    INCLUDEPATH+=/usr/include/thumbnailer-qt-1.0/unity/thumbnailer/qt \
     TELEGRAMQML_INCLUDE_PATH=$TG_INCS/telegramqml \
-    PREFIX=/usr BUILD_MODE+=lib DEFINES+=UBUNTU_PHONE -r .. QMAKE_CFLAGS_ISYSTEM= || exit 1
-#    PREFIX=/usr BUILD_MODE+=lib DEFINES+=UBUNTU_PHONE -r .. QMAKE_CFLAGS_ISYSTEM= CONFIG+=debug || exit 1 //for debugging
+    PREFIX=/ BUILD_MODE+=lib DEFINES+=UBUNTU_PHONE -r .. QMAKE_CFLAGS_ISYSTEM= || exit 1
+#    PREFIX=/ BUILD_MODE+=lib DEFINES+=UBUNTU_PHONE -r .. QMAKE_CFLAGS_ISYSTEM= CONFIG+=debug || exit 1 //for debugging
 $MAKE_BIN -j4 || exit 1
 $MAKE_BIN INSTALL_ROOT=$CLICK_TARGET_DIR install || exit 1
-cd $TG_DIR
+
 }
 
 build_telegram() {
-	mkdir -p $TELEGRAM_SOURCES/build
-	cd $TELEGRAM_SOURCES/build
-	cmake .. \
-	      -GNinja \
-	      -DCMAKE_MAKE_PROGRAM=/usr/bin/ninja \
-	      -DCMAKE_PREFIX_PATH="${CLICK_TARGET_DIR}" \
-	      -DCMAKE_INSTALL_PREFIX="${CLICK_TARGET_DIR}" \
-	      -DI18N=0 \
-	      -DUBUNTU_TOUCH=1 \
-	      -DCLICK_ARCH="${ARCH}"
 
-	ninja install
+echo "*****************************************"
+echo "Building Telegram App"
+echo "*****************************************"
+
+cd $TELEGRAM_SOURCES/telegram
+mkdir -p $BUILD_DIR_BASENAME/po && cd $BUILD_DIR_BASENAME || exit 1
+$QMAKE_BIN LIBS+=-L$CLICK_TARGET_DIR/lib/x86_64-linux-gnu INCLUDEPATH+=/include/thumbnailer-qt-1.0/unity/thumbnailer/qt INCLUDEPATH+=$CLICK_TARGET_DIR/include/libqtelegram-ae INCLUDEPATH+=$CLICK_TARGET_DIR/include/telegramqml PREFIX=/ -r .. || exit 1
+# $QMAKE_BIN LIBS+=-L$TH_LIBS LIBS+=-L$TG_LIBS INCLUDEPATH+=$TH_INCS INCLUDEPATH+=$TG_INCS/libqtelegram-ae INCLUDEPATH+=$TG_INCS/telegramqml PREFIX=/ -r .. CONFIG+=debug || exit 1 //for debugging
+#LIBS+=-lunity-scopes INCLUDEPATH+=/usr/include/unity-scopes-0 -r ..
+$MAKE_BIN -j4 || exit 1
+$MAKE_BIN INSTALL_ROOT=$CLICK_TARGET_DIR install || exit 1
+
 }
 
 cleanup_click_dir() {
+
+echo "*****************************************"
+echo "Cleaning up"
+echo "*****************************************"
+
 	# Strip out documentation and includes
-	rm -r \
-		$CLICK_TARGET_DIR/usr \
-		$CLICK_TARGET_DIR/include \
-		$CLICK_TARGET_DIR/share/doc \
-		$CLICK_TARGET_DIR/share/locale \
-		$CLICK_TARGET_DIR/lib/$DEB_HOST_MULTIARCH/cmake \
-		$CLICK_TARGET_DIR/lib/$DEB_HOST_MULTIARCH/pkgconfig \
-		$CLICK_TARGET_DIR/lib/$DEB_HOST_MULTIARCH/qt5/mkspecs
+	rm -r $CLICK_TARGET_DIR/include
 }
 
-echo "*****************************************"
-echo "Downloading Debian packages"
-echo "*****************************************"
-
-install_dependencies
-
-
-echo "*****************************************"
-echo "Building Telegram"
-echo "*****************************************"
-
-/usr/bin/qt5-qmake-arm-linux-gnueabihf -version
-build_libqtelegram && build_TelegramQML && build_telegram
-
-cleanup_click_dir
+build_libqtelegram && build_TelegramQML && build_telegram && cleanup_click_dir
 
 echo "*****************************************"
 echo "Build script finished, now leaving work to 'click build'"
